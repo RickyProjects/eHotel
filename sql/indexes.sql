c4d7e2e0ed13e6609d3efb872b94b76d02ffcdbe
vravi014 <vravi014@uottawa.ca>
2026-04-04 18:01:02 -0400

    Added room search index

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -1,5 +1,8 @@
 CREATE INDEX idx_room_price ON room(price);
 
+CREATE INDEX idx_room_search
+ON room(hotel_id, price, room_capacity);
+
 CREATE INDEX idx_booking_room_dates
 ON booking(hotel_id, room_number, start_date, end_date);
 
